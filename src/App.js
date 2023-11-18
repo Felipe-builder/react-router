5f3c4f610f6e40c98c8f4ba9e675db3cbca4e2e5
Felipe Vasconcelos <felipe.vasconcelos@semantix.ai>
2023-11-18 15:21:24 -0300

    Nested routes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Product from './pages/Product';
+import Info from './pages/Info';
 
 // 1 - config react router
 
@@ -18,6 +19,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/about' element={<About />}/>
+          <Route path='/products/:id/info' element={<Info />}/>
           <Route path='/products/:id' element={<Product />}/>
         </Routes>
       </BrowserRouter>
