6f61e76559b999d3d314231cf328e5236a26df90
Felipe Vasconcelos <felipe.vasconcelos@semantix.ai>
2023-11-20 12:24:19 -0300

    Search Params

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
+import SearchForm from './components/SearchForm';
 
 import Home from './pages/Home';
 import About from './pages/About';
 import Product from './pages/Product';
 import Info from './pages/Info';
 import NotFound from './pages/NotFound';
+import Search from './pages/Search'
 
 // 1 - config react router
 
@@ -17,13 +19,14 @@ function App() {
       <h1>React Router</h1>
       <BrowserRouter>
         <Navbar />
+        <SearchForm />
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/about' element={<About />}/>
           <Route path='/products/:id/info' element={<Info />}/>
           <Route path='/products/:id' element={<Product />}/>
+          <Route path='/search' element={<Search />} />
           <Route path='*' element={<NotFound />}/>
-
         </Routes>
       </BrowserRouter>
     </div>
